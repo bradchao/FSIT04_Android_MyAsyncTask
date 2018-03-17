538ad86a88b768d5e734b145cca1995c4855cc69
Brad <brad@brad.tw>
2018-03-17 11:31:26 +0800

    v2

--- app/src/main/java/tw/org/iii/myasynctask/MainActivity.java
+++ app/src/main/java/tw/org/iii/myasynctask/MainActivity.java
@@ -5,29 +5,57 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     private MyAsyncTask myAsyncTask;
+    private TextView tv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tv = findViewById(R.id.tv);
+
     }
 
     public void test1(View view) {
         Log.v("brad", "before");
         myAsyncTask = new MyAsyncTask();
-        myAsyncTask.execute();
+        myAsyncTask.execute("Brad", "Tony","Mary");
         Log.v("brad", "after");
     }
 
-    private class MyAsyncTask extends AsyncTask<Void, Void, Void>{
+    private class MyAsyncTask extends AsyncTask<String, Integer, String>{
 
         @Override
-        protected Void doInBackground(Void... voids) {
+        protected String doInBackground(String... names) {
             Log.v("brad", "doInBackground");
-            return null;
+
+            for (String name : names){
+                Log.v("brad", "Hello, " + name);
+                //tv.setText(name);
+
+                int rand = (int)(Math.random()*49+1);
+                publishProgress(rand, rand*10);
+
+                try {
+                    Thread.sleep(1*1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+
+
+            return "OK";
+        }
+
+        @Override
+        protected void onProgressUpdate(Integer... values) {
+            super.onProgressUpdate(values);
+            Log.v("brad", "onProgressUpdate");
+            tv.setText(values[0] + ":" + values[1]);
         }
 
         @Override
@@ -37,21 +65,18 @@ public class MainActivity extends AppCompatActivity {
         }
 
         @Override
-        protected void onPostExecute(Void aVoid) {
-            super.onPostExecute(aVoid);
-            Log.v("brad", "onPostExecute");
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+            Log.v("brad", "onPostExecute:" + result);
+            tv.setText(result);
         }
 
-        @Override
-        protected void onProgressUpdate(Void... values) {
-            super.onProgressUpdate(values);
-            Log.v("brad", "onProgressUpdate");
-        }
+
 
         @Override
-        protected void onCancelled(Void aVoid) {
-            super.onCancelled(aVoid);
-            Log.v("brad", "onCancelled(Void aVoid)");
+        protected void onCancelled(String result) {
+            super.onCancelled(result);
+            Log.v("brad", "onCancelled(Void aVoid):" + result);
         }
 
         @Override
